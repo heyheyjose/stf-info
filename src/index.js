3d0872ea6fe4250d233212b6618859d2bab7d224
Jose Vazquez <vazquez.jose.f@gmail.com>
2017-10-18 23:11:28 -0400

    stopped registering service worker... this is unnecessary at the moment

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
 
 render(<App />, document.getElementById('root'));
 
-registerServiceWorker();
+// registerServiceWorker();
