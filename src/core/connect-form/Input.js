5c1359c26a985596e9ec30813c73c28d4d5c7b7a
Jose Vazquez <josevazquez7@deloitte.com>
2017-10-23 15:29:46 -0400

    added required prop + proptypes and default props

--- src/core/connect-form/Input.js
+++ src/core/connect-form/Input.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Input = props => {
   return (
@@ -6,9 +7,22 @@ const Input = props => {
       <label className="connect-form-label" htmlFor={props.htmlId}>
         {props.label}
       </label>
-      <input id={props.htmlId} type={props.type} />
+      <input id={props.htmlId} type={props.type} name={props.name} required={props.required} />
     </div>
   );
 };
 
 export default Input;
+
+Input.defaultProps = {
+  name: '',
+  required: false,
+};
+
+Input.propTypes = {
+  name: PropTypes.string,
+  required: PropTypes.bool,
+  htmlId: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+};
