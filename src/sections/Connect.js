89275f67c1bd48c35335cb101d61d0eaffa26e18
Jose Vazquez <josevazquez7@deloitte.com>
2017-10-23 15:49:10 -0400

    getting form in place

--- src/sections/Connect.js
+++ src/sections/Connect.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import classNames from 'classnames';
 import { Collapse } from 'reactstrap';
 
-// import Input from '../core/connect-form/Input';
+import Input from '../core/connect-form/Input';
 
 class Connect extends Component {
   constructor(props) {
@@ -29,63 +29,24 @@ class Connect extends Component {
         </div>
         <Collapse isOpen={this.state.show}>
           <div className="content-section-connect">
-            <p style={{ marginBottom: 30 }}>
-              Fill out a connection card so we can better understand how we can
-              serve you!
-            </p>
-            <a
-              href="https://stfc.fellowshiponego.com/external/form/bc536a3a-12e9-467b-b798-de246250254c"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="btn btn-outline-primary btn-lg btn-connect-card"
-            >
-              Connection Card
-            </a>
-            {/*}
             <h5 className="content-section-connect-title">
-              Fill out the connection card below:
+              Fill out a connection card so we can better understand how we can serve you!
             </h5>
-            <Input
-              type="text"
-              htmlId="connect-input-first-name"
-              label="First Name"
-            />
-            <Input
-              type="text"
-              htmlId="connect-input-last-name"
-              label="Last Name"
-            />
-            <Input type="email" htmlId="connect-input-email" label="Email" />
-            <Input
-              type="tel"
-              htmlId="connect-input-phone"
-              label="Phone Number (optional)"
-            />
-            <label
-              className="connect-form-label-contact"
-              htmlFor="connect-input-contact-method"
-            >
-              Preferred Method of Contact (optional)
-            </label>
-            <div style={{ textAlign: 'center', marginBottom: 10 }}>
-              <ButtonGroup size="lg">
-                <Button outline>Email</Button>
-                <Button outline>Phone</Button>
-              </ButtonGroup>
-            </div>
-            <label
-              className="connect-form-label"
-              htmlFor="connect-input-request"
-            >
-              Prayer Requests (optional)
-            </label>
-            <textarea id="connect-input-request" />
-            <div style={{ textAlign: 'center', margin: 20 }}>
-              <Button outline size="lg" color="success">
-                Submit
-              </Button>
-            </div>
-            {*/}
+            <form method="POST" action="http://formspree.io/jose.vazquez.dev+formspree@gmail.com">
+              <Input type="text" htmlId="connect-input-name" label="Name" name="Name" required />
+              <Input type="email" htmlId="connect-input-email" label="Email" name="_reply" required />
+              <Input type="tel" htmlId="connect-input-phone" label="Phone (optional)" name="Phone" />
+              <label className="connect-form-label" htmlFor="connect-input-request">
+                Prayer Requests (optional)
+              </label>
+              <textarea id="connect-input-request" name="Prayer Request" />
+              <input type="hidden" name="_next" value="/" />
+              <div style={{ textAlign: 'center', margin: 20 }}>
+                <button type="submit" className="btn btn-outline-primary btn-lg btn-connect-card">
+                  Submit
+                </button>
+              </div>
+            </form>
           </div>
         </Collapse>
       </div>
