e73a50578f364faf070184f025df1e53bf560527
Jose Vazquez <josevazquez7@deloitte.com>
2017-10-24 12:16:46 -0400

    updated links to F1 forms to be direct, since the forms can be set to redirect back

--- src/sections/Connect.js
+++ src/sections/Connect.js
@@ -30,62 +30,14 @@ class Connect extends Component {
         <Collapse isOpen={this.state.show}>
           <div className="content-section-connect">
             <p style={{ marginBottom: 30 }}>
-              Fill out a connection card so we can better understand how we can
-              serve you!
+              Fill out a connection card so we can better understand how we can serve you!
             </p>
             <a
               href="https://stfc.fellowshiponego.com/external/form/bc536a3a-12e9-467b-b798-de246250254c"
-              target="_blank"
-              rel="noopener noreferrer"
               className="btn btn-outline-primary btn-lg btn-connect-card"
             >
               Connection Card
             </a>
-            {/*}
-            <h5 className="content-section-connect-title">
-              Fill out the connection card below:
-            </h5>
-            <Input
-              type="text"
-              htmlId="connect-input-first-name"
-              label="First Name"
-            />
-            <Input
-              type="text"
-              htmlId="connect-input-last-name"
-              label="Last Name"
-            />
-            <Input type="email" htmlId="connect-input-email" label="Email" />
-            <Input
-              type="tel"
-              htmlId="connect-input-phone"
-              label="Phone Number (optional)"
-            />
-            <label
-              className="connect-form-label-contact"
-              htmlFor="connect-input-contact-method"
-            >
-              Preferred Method of Contact (optional)
-            </label>
-            <div style={{ textAlign: 'center', marginBottom: 10 }}>
-              <ButtonGroup size="lg">
-                <Button outline>Email</Button>
-                <Button outline>Phone</Button>
-              </ButtonGroup>
-            </div>
-            <label
-              className="connect-form-label"
-              htmlFor="connect-input-request"
-            >
-              Prayer Requests (optional)
-            </label>
-            <textarea id="connect-input-request" />
-            <div style={{ textAlign: 'center', margin: 20 }}>
-              <Button outline size="lg" color="success">
-                Submit
-              </Button>
-            </div>
-            {*/}
           </div>
         </Collapse>
       </div>
