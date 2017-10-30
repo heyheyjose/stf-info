215f6eb8c60e7910cdb23f0a164438cb16006245
Jose Vazquez <josevazquez7@deloitte.com>
2017-10-30 15:11:24 -0400

    finishing up a first draft idea of contact us page

--- src/sections/Contact.js
+++ src/sections/Contact.js
@@ -3,7 +3,9 @@ import classNames from 'classnames';
 import { Collapse } from 'reactstrap';
 
 import CampusInfoCard from '../core/CampusInfoCard';
-import ballastPointImg from '../images/ballast.png';
+import ballastPointImg from '../images/ballast_copy.png';
+import davisIslandsImg from '../images/islands_copy.png';
+import channelDistrictImg from '../images/channel_copy.png';
 
 class Contact extends Component {
   constructor(props) {
@@ -11,7 +13,7 @@ class Contact extends Component {
 
     this.state = {
       show: false,
-      showBpInfo: false,
+      showBpInfo: true,
       showDiInfo: false,
       showCdInfo: false,
       bpCampusInfo: {
@@ -22,37 +24,46 @@ class Contact extends Component {
         serviceTimes: 'Sundays, 9:15 a.m. and 11 a.m.',
       },
       diCampusInfo: {
-        image: ballastPointImg,
-        title: 'Ballast Point Campus',
-        address1: '5101 Bayshore Boulevard',
-        address2: 'Tampa, Florida 33611',
-        serviceTimes: 'Sundays, 9:15 a.m. and 11 a.m.',
+        image: davisIslandsImg,
+        title: 'Davis Islands Campus',
+        address1: '97 Biscayne Avenue',
+        address2: 'Tampa, Florida 33606',
+        serviceTimes: 'Sundays, 10:30 a.m.',
       },
       cdCampusInfo: {
-        image: ballastPointImg,
-        title: 'Ballast Point Campus',
-        address1: '5101 Bayshore Boulevard',
-        address2: 'Tampa, Florida 33611',
-        serviceTimes: 'Sundays, 9:15 a.m. and 11 a.m.',
+        image: channelDistrictImg,
+        title: 'Channel District Campus',
+        address1: '1120 E. Kennedy Boulevard, #151',
+        address2: 'Tampa, Florida 33602',
+        serviceTimes: 'Sundays, 11 a.m.',
       },
     };
 
     this.toggle = this.toggle.bind(this);
-    this.toggleCampusCard = this.toggleCampusCard.bind(this);
+    this.onChange = this.onChange.bind(this);
   }
 
   toggle() {
     this.setState({ show: !this.state.show });
   }
 
-  toggleCampusCard(e) {
-    // console.log(e.target);
-    if (e.target.innerText === 'Ballast Point Info') {
-      this.setState({ showBpInfo: !this.state.showBpInfo });
-    } else if (e.target.innerText === 'Davis Islands Info') {
-      this.setState({ showDiInfo: !this.state.showDiInfo });
-    } else if (e.target.innerText === 'Channel District Info') {
-      this.setState({ showCdInfo: !this.state.showCdInfo });
+  onChange(e) {
+    if (e.target[0].selected) {
+      this.setState({ showBpInfo: !this.state.showBpInfo, showDiInfo: false, showCdInfo: false });
+    } else if (e.target[1].selected) {
+      this.setState({ showDiInfo: !this.state.showDiInfo, showBpInfo: false, showCdInfo: false });
+    } else if (e.target[2].selected) {
+      this.setState({ showCdInfo: !this.state.showCdInfo, showBpInfo: false, showDiInfo: false });
+    }
+  }
+
+  showCampusInfo() {
+    if (this.state.showBpInfo) {
+      return <CampusInfoCard campusInfo={this.state.bpCampusInfo} />;
+    } else if (this.state.showDiInfo) {
+      return <CampusInfoCard campusInfo={this.state.diCampusInfo} />;
+    } else if (this.state.showCdInfo) {
+      return <CampusInfoCard campusInfo={this.state.cdCampusInfo} />;
     }
   }
 
@@ -69,37 +80,22 @@ class Contact extends Component {
         <Collapse isOpen={this.state.show}>
           <div className="content-section-contact">
             <h4 style={{ marginBottom: 10 }}>Get in touch with us!</h4>
-            <p style={{ marginBottom: 30 }}>Phone: 813-251-1515</p>
-            <a
-              href="http://stfchurch.com/contact"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="btn btn-outline-primary btn-lg btn-contact"
-            >
-              Contact Us
-            </a>
-            <div style={{ margin: 20 }} />
-            <button onClick={this.toggleCampusCard} className="btn btn-outline-primary btn-lg btn-contact">
-              Ballast Point Info
-            </button>
-            <div style={{ margin: 20 }} />
-            <button onClick={this.toggleCampusCard} className="btn btn-outline-primary btn-lg btn-contact">
-              Davis Islands Info
-            </button>
-            <div style={{ margin: 20 }} />
-            <button onClick={this.toggleCampusCard} className="btn btn-outline-primary btn-lg btn-contact">
-              Channel District Info
-            </button>
+            <p style={{ marginBottom: 10 }}>
+              Main Office:
+              <a style={{ textDecoration: 'underline', marginLeft: 10 }} href="tel:+1-813-251-1515">
+                813-251-1515
+              </a>
+            </p>
+            <p style={{ fontSize: 14 }}>{this.state.diCampusInfo.address1}</p>
+            <p style={{ fontSize: 14, marginBottom: 40 }}>{this.state.diCampusInfo.address2}</p>
 
-            <Collapse isOpen={this.state.showBpInfo}>
-              <CampusInfoCard campusInfo={this.state.bpCampusInfo} />
-            </Collapse>
-            <Collapse isOpen={this.state.showDiInfo}>
-              <CampusInfoCard campusInfo={this.state.diCampusInfo} />
-            </Collapse>
-            <Collapse isOpen={this.state.showCdInfo}>
-              <CampusInfoCard campusInfo={this.state.cdCampusInfo} />
-            </Collapse>
+            <p style={{ marginBottom: 5 }}>Select for campus info:</p>
+            <select onChange={this.onChange} className="content-section-contact-campus-selector">
+              <option>Ballast Point</option>
+              <option>Davis Islands</option>
+              <option>Channel District</option>
+            </select>
+            {this.showCampusInfo()}
           </div>
         </Collapse>
       </div>
