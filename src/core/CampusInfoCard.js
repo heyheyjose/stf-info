d2888b727f1aeb163c5e7d65c51c31bb664ac6a0
Jose Vazquez <josevazquez7@deloitte.com>
2017-10-30 16:34:04 -0400

    added some padding to the info card

--- src/core/CampusInfoCard.js
+++ src/core/CampusInfoCard.js
@@ -6,13 +6,15 @@ const CampusInfoCard = props => {
   return (
     <div className="campus-info-card-wrapper">
       <img alt={props.campusInfo.title} src={props.campusInfo.image} className="campus-info-card-img" />
-      <h4 className="campus-info-card-title">{props.campusInfo.title}</h4>
-      <p className="campus-info-card-text">{props.campusInfo.address1}</p>
-      <p className="campus-info-card-text">{props.campusInfo.address2}</p>
-      <p className="campus-info-card-text">
-        <strong>Service Times: </strong>
-        {props.campusInfo.serviceTimes}
-      </p>
+      <div className="campus-info-card-content-wrapper">
+        <h4 className="campus-info-card-title">{props.campusInfo.title}</h4>
+        <p className="campus-info-card-text">{props.campusInfo.address1}</p>
+        <p className="campus-info-card-text">{props.campusInfo.address2}</p>
+        <p className="campus-info-card-text">
+          <strong>Service Times: </strong>
+          {props.campusInfo.serviceTimes}
+        </p>
+      </div>
     </div>
   );
 };
